9a825309baad218951619689061f04c6a8a44f5a
bhavani-verasangi <bhavaniverasangi410@gmail.com>
2023-05-17 10:47:46 +0000

    commited

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div className="congrats-card-container">
+    <h1 className="title">Congratulations</h1>
+    <div className="card-container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2>Bhavani V</h2>
+      <p>Vijaya Engineering College, Khammam</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
